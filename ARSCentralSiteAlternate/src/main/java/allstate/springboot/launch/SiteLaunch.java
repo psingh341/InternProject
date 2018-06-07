bdd9ec41e2555cbe6f9ca436fe6fc88c2af1a36d
Prajesh Singh <psingh8@ncsu.edu>
2018-06-07 10:17:48 -0400

    Initial Commit to repository

---
+++ ARSCentralSiteAlternate/src/main/java/allstate/springboot/launch/SiteLaunch.java
@@ -0,0 +1,13 @@
+package allstate.springboot.launch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteLaunch {
+
+    public static void main ( final String[] args ) {
+        SpringApplication.run( SiteLaunch.class, args );
+    }
+
+}
